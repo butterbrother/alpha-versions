1217d6ddc3011bbd8f13dea23a067fd2f4ce3503
Oleg Bobukh <o.bobukh@yandex.ru>
2015-07-23 23:56:43 +0300

    GUI layout learning

--- learn/java/Applets/GridBagDemo.java
+++ learn/java/Applets/GridBagDemo.java
@@ -2,6 +2,9 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GridBagDemo extends Frame {
+	private GridBagConstraints gbc, special;
+	private TextField Tgridx, Tgridy;
+
 	public static void main(String args[]) {
 		new GridBagDemo();
 	}
@@ -10,4 +13,35 @@ public class GridBagDemo extends Frame {
 		super("Пример работы GridBagLayout");
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
-				System.
+				System.exit(0);
+			}
+		});
+
+		setLayout(new GridBagLayout());
+		gbc = new GridBagConstraints();
+
+		special = new GridBagConstraints();
+		// Расположение в ячейке
+		special.gridx = 0;
+		special.gridy = 3;
+
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		add(new Label("gridx:", Label.RIGHT), gbc);
+
+		gbc.gridx = 1;
+		gbc.weightx = 0.5;
+		Tgridx = new TextField(Integer.toString(special.gridx));
+		add(Tgridx, gbc);
+
+		gbc.gridx = 4;
+		gbc.weightx = 0.0;
+		add(new Label("gridy:", Label.RIGHT), gbc);
+
+		gbc.gridx = 5;
+
+		setVisible(true);
+		setSize(getLayout().preferredLayoutSize(this));
+	}
+}
